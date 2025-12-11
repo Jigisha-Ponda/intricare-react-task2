9e39770b890020f24020e998368da8b7f6bcd270
Jigisha Ponda <jigishaadatiya21@gmail.com>
2025-12-11 16:03:01 +0530

    added tasks floating lable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import HeroSection from "./components/HeroSection";
+import { BrowserRouter as Router, Routes, Route, NavLink, Navigate } from "react-router-dom";
+import FigmaDesign from "./components/FigmaDesign";
 
 function App() {
   return (
     <div className="=w-screen">
-      <Navbar/>
-      <HeroSection/>
+      <Router>
+        <div className="min-h-screen">
+          <div class="position-fixed top-50 start-0 p-3 border bg-white translate-middle-y d-flex flex-column gap-3 z-3">
+
+            <a
+              href="https://intricare-react-task.vercel.app/"
+              class="px-4 py-2 rounded fw-semibold text-decoration-none text-primary bg-blue hover-bg-primary"
+            >
+              Task 1
+            </a>
+
+            <a
+              href="https://intricare-react-task2.vercel.app/"
+              class="px-4 py-2 rounded fw-semibold text-decoration-none text-success bg-green hover-bg-success"
+            >
+              Task 2
+            </a>
+
+          </div>
+
+
+          <Routes>
+            <Route path="https://intricare-react-task.vercel.app/" />
+            <Route path="/" Component={FigmaDesign} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/FigmaDesign.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar";
+import HeroSection from "./HeroSection";
+
+function FigmaDesign() {
+  return (
+    <div className="=w-screen">
+      <Navbar/>
+      <HeroSection/>
+    </div>
+  );
+}
+
+export default FigmaDesign;
